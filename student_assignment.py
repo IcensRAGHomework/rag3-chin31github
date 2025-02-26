dda1e29ddd188082a05c7ecb0b028c79865db5aa
chin31github <tclin.lin@gmail.com>
2025-02-26 15:15:27 +0800

    Add files via upload

--- student_assignment.py
+++ student_assignment.py
@@ -6,12 +6,44 @@ from chromadb.utils import embedding_functions
 
 from model_configurations import get_model_configuration
 
+#must comment rich when upload to homework
+#from rich import print as pprint
+
 gpt_emb_version = 'text-embedding-ada-002'
 gpt_emb_config = get_model_configuration(gpt_emb_version)
 
 dbpath = "./"
 
+import csv
 def generate_hw01():
+    chroma_client = chromadb.PersistentClient(path=dbpath)
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key = gpt_emb_config['api_key'],
+        api_base = gpt_emb_config['api_base'],
+        api_type = gpt_emb_config['openai_type'],
+        api_version = gpt_emb_config['api_version'],
+        deployment_id = gpt_emb_config['deployment_name']
+    )
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL",
+        metadata={"hnsw:space": "cosine"},
+        embedding_function=openai_ef
+    )
+    with open('COA_OpenData.csv', encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for index, row in enumerate(reader):
+            print({index}, row["Name"])
+            collection.add(
+                ids=[f"id{index}"],
+                documents=[
+                    row["HostWords"]
+                ],
+                metadatas=[
+                    {"file_name": "COA_OpenData.csv", "name": row["Name"], "type": row["Type"], "address": row["Address"], "tel": row["Tel"], "city": row["City"], "town": row["Town"], "date": row["CreateDate"]},
+                ]
+            )
+
+    return collection
     pass
     
 def generate_hw02(question, city, store_type, start_date, end_date):
@@ -34,5 +66,7 @@ def demo(question):
         metadata={"hnsw:space": "cosine"},
         embedding_function=openai_ef
     )
-    
     return collection
+    pass
+
+#pprint(generate_hw01())
