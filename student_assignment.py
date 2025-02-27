4776374ac7a6bd45559fde12029770121afb8fe4
chin31github <tclin.lin@gmail.com>
2025-02-27 15:23:54 +0800

    Add files via upload

--- student_assignment.py
+++ student_assignment.py
@@ -17,7 +17,55 @@ dbpath = "./"
 import csv
 import datetime
 import time
+import pandas as pd
+
 def generate_hw01():
+    # 連接地端的database
+    chroma_client = chromadb.PersistentClient(path=dbpath)
+    # 建立embedding function
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key = gpt_emb_config['api_key'],
+        api_base = gpt_emb_config['api_base'],
+        api_type = gpt_emb_config['openai_type'],
+        api_version = gpt_emb_config['api_version'],
+        deployment_id = gpt_emb_config['deployment_name']
+    )
+    # 建立collection
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL",
+        metadata={"hnsw:space": "cosine"},
+        embedding_function=openai_ef
+    )
+
+    if collection.count() == 0:
+        # 讀取CSV檔案
+        csv_file_name = "COA_OpenData.csv"
+        df = pd.read_csv(csv_file_name)
+        print("columns: "+df.columns)
+
+        for idx, row in df.iterrows():
+            metadata = {
+                "file_name": csv_file_name,
+                "name": row["Name"],
+                "type": row["Type"],
+                "address": row["Address"],
+                "tel": row["Tel"],
+                "city": row["City"],
+                "town": row["Town"],
+                "date": int(datetime.datetime.strptime(row['CreateDate'], '%Y-%m-%d').timestamp())  # 轉timeStamp
+            }
+            print(str(idx)+str(metadata))
+            print("\n")
+            # 將資料寫入 ChromaDB
+            collection.add(
+                ids=[str(idx)],
+                metadatas=[metadata],
+                documents=[row["HostWords"]]
+            )
+    return collection
+    pass
+    
+def generate_hw08():
     chroma_client = chromadb.PersistentClient(path=dbpath)
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
         api_key = gpt_emb_config['api_key'],
